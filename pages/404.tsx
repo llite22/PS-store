491215d7b8600aaa71d14becabb77e3be18ab549
llite22 <llite2@yandex.ru>
2023-05-06 11:14:06 +0300

    поправил 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,13 +3,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const NotFound: NextPage = () => {
+  const imageStyle = {
+    maxWidth: '100%',
+    height: 'auto',
+  }
   return (
     <>
     <Head>
         <title>Not Found</title>
     </Head>
     <div>
-        <Image src='/images/404.jpg' width={1440} height={740} alt="404"/>
+        <Image style={imageStyle} src='/images/404.jpg' width={1440} height={740} alt="404"/>
     </div>
     </>
   )
